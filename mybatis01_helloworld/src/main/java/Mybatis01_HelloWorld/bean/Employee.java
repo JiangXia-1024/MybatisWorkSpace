20c02e1499a020a4865c7ba38fd7108a39b6b648
jiangxia <j17621607346@163.com>
2022-02-04 00:00:27 +0800

    mybatis基础入门

---
+++ mybatis01_helloworld/src/main/java/Mybatis01_HelloWorld/bean/Employee.java
@@ -0,0 +1,62 @@
+package Mybatis01_HelloWorld.bean;
+
+/**
+ * @author jiangxia
+ * @date 2022年02月02日 14:39
+ * desc: employee实体类
+ */
+public class Employee {
+    private Integer id;
+    private String name;
+    private String gender;
+    private String email;
+
+    public Employee(Integer id, String name, String gender, String email) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
